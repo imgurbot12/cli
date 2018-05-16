754ccb270983895604be252649e5de05ddbead20
IHaxYou <andrewcscott007@gmail.com>
2018-05-15 20:37:00 -0700

    this is now a python library FOR REALS

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cli',
+    version='1.0.0',
+    packages=['cli'],
+    url='https://github.com/imgurbot12/cli',
+    license='MIT License',
+    author='imgurbot12',
+    author_email='',
+    description='A simple, fast, and fun package for building command line apps in Python'
+)
