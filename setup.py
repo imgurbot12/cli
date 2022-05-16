184efef8706636fd90cef70e9310b92808b006e9
imgurbot12 <imgurbot12@gmail.com>
2022-05-16 13:12:15 -0700

    feat: upgrade version w/ bugfix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cli',
-    version='2.0.0',
+    version='2.0.1',
     packages=['cli'],
     url='https://github.com/imgurbot12/cli',
     license='MIT License',
