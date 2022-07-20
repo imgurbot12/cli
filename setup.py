aab340de89f7d6f95be0e5eca2baadb59682f3e8
imgurbot12 <imgurbot12@gmail.com>
2022-07-20 13:27:37 -0700

    fix: update version number w/ changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cli',
-    version='2.0.1',
+    version='2.0.2',
     packages=['cli'],
     url='https://github.com/imgurbot12/cli',
     license='MIT License',
